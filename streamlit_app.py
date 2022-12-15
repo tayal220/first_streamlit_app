90812d0be95a90b51315ef278246e64619884b14
HIMANSHU TAYAL <tayal220@gmail.com>
2022-12-16 01:40:16 +0530

    Update streamlit_app.py
    Made it according to instructions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 
-streamlit.title('Beginning a new process')
-streamlit.header('Snowflake basket addition')
-
-streamlit.text('Learning git integration with streamlit')
+streamlit.title('My Mom's new healthy dinner')
+streamlit.header('Breakfast Favourite')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')      
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')                
